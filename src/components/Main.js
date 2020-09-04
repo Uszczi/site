8c2af9ead7e2af771c415af5410073db9d80ad3d
Uszczi <mateuszpapuga24@gmail.com>
2020-09-05 00:45:39 +0200

    add p5 demo

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Dino from "../projects/dino/App";
+import Demo from "../projects/p5demo/App";
 
 function Main() {
   return (
     <div className="Main">
+      <Demo />
       <Dino />
     </div>
   );
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Footer() {
-  return <div className="Footer">Footer</div>;
+  return <div className="Footer">Footer tmp </div>;
 }
 
 export default Footer;
